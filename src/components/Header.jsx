ca0b5c571b44cdfc180733fce039344addadbbc1
Maxime <maxime.drouhin71@gmail.com>
2025-09-14 18:28:49 +0200

    added header link underline when matching the current location

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 import "../styles/Header.scss";
 
 function Header() {
+	const location = useLocation();
 	return (
 		<header className="container">
 			<img src={logo} alt="" className="container_logo" />
 			<nav className="container_link">
-				<Link to="/" className="container_link--home">
+				<Link
+					to="/"
+					className={`container_link--home${
+						location.pathname === "/" ? " selected" : ""
+					}`}
+				>
 					Accueil
 				</Link>
-				<Link to="/about" className="container_link--about">
+				<Link
+					to="/about"
+					className={`container_link--about${
+						location.pathname === "/about" ? " selected" : ""
+					}`}
+				>
 					A Propos
 				</Link>
 			</nav>
